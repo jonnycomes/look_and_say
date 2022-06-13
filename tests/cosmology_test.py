427cd073319eecfeba40bd6aba826075b855908c
Jonny Comes <jonnycomes@gmail.com>
2022-06-12 21:58:44 -0600

    Adding unit test for decay_tree with transuranic elements.

--- tests/cosmology_test.py
+++ tests/cosmology_test.py
@@ -5,6 +5,11 @@ def test_decay_tree():
     cosmo = Cosmology()
     assert cosmo.decay_tree('22233') == {'22233': ['3', '22', '3']}
 
+def test_decay_tree_transuranic():
+    '''Testing the decay_tree method on 5'''
+    cosmo = Cosmology(digits='123456789')
+    assert cosmo.decay_tree('5') == {'5': [{'15': [{'1115': [{'3115': [{'132115': [{'1113122115': [{'311311222115': ['1321132', {'1322115': [{'1113222115': [{'3113322115': ['132', {'123222115': [{'111213322115': [{'31121123222115': [{'132112211213322115': [{'111312212221121123222115': [{'3113112211322112211213322115': [{'1321132122211322212221121123222115': [{'111312211312113221133211322112211213322115': ['31131122211311122113222', '12', '312211322212221121123222115']}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}
+
 
 def test_cosmology_proof(capsys):
     '''
@@ -15,3 +20,5 @@ def test_cosmology_proof(capsys):
     cosmo.proof()
     captured = capsys.readouterr()
     assert 'bound of 27 days for the age of an exotic element' in captured.out
+
+
